857db761e093f972bc9336aafb87ca60fcc1425f
adilfaiz001 <adilfaiz01@live.com>
2018-10-09 00:36:53 +0530

    naming and structuring

--- bot1.js
+++ bot1.js
@@ -1,96 +1,58 @@
-var Twit=require('twit');
-var fs =require('fs');
+const Twit=require('twit');
+const  fs =require('fs');
 
 
-var config= require('./config');
+const config= require('./config');
+const t = new Twit(config);
 
+//get()
 
-
-var t = new Twit(config);
-
-trends();
-
-function trends(){
-  console.log("trend called")
-var params={id:'1',count:3}
-
-
+var params={id:'1',count:5}
 t.get('trends/place',params,function(err,data,res){
-
-    var topt=JSON.stringify(data, undefined, 2);
-
-WRITEFILE(topt)
-
-
-
-
-
-
-
-})
-
-}
-
-
-function WRITEFILE(topt){
-fs.writeFile("trend.json", topt, function(err) {
-    if(err) {
-        return console.log(err);
-    }
-
-
-    console.log("The file was saved!");
-
+  var top_twit=JSON.stringify(data, undefined, 2);
+  twit_stream = fs.createWriteStream('trends.json');
+  twit_stream.write(top_twit);
 });
+
+
+
+
+/*=============================================================================/
+/*-------------------------Twits Check-----------------------------------------*/
+trending = require('./trends.json');
+console.log(trending[0].trends[0]);
+//=============================================================================//
+
+
+
+
+
+function posting()
+{
+  var tweet={status:'#getting started again'};
+  function tweeted(err,data2,res)
+  {
+    console.log(data2);
+  }
+  t.post('statuses/update',tweet,tweeted);
 }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-function posting(){
-var tweet={status:'#getting started again'};
-function tweeted(err,data2,res){
-  console.log(data2);
+function get()
+{
+    t.get('search/tweets',{q:'aman',result_type:'popular',count:5},function(err,data1,res){
+      if(err){
+        console.log('error');
+      }
+      else
+      {
+        var nec=data1.statuses;
+        for (var i=0;i<nec.length;i++)
+        {
+          console.log(nec[i].text)
+          console.log(nec[i].created_at)
+        }
+      }
+    });
 }
-
-t.post('statuses/update',tweet,tweeted);
-}
-
-
-
-function get(){
-t.get('search/tweets',{q:'#FelizLunes',result_type:'popular',count:5},function(err,data1,res){
-  if(err){
-    console.log('error')}
-    else{
-    var nec=data1.statuses;
-    for (var i=0;i<nec.length;i++){
-      console.log(nec[i].text)
-      console.log(nec[i].created_at)
-
-    }
-
-    }
-
-})
-}
-*/
