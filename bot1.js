3e1ecd633412f87b07cf4e625c60975541073581
ashad15 <abbasashad15@gmail.com>
2018-10-09 20:34:19 +0530

    msg

--- bot1.js
+++ bot1.js
@@ -7,7 +7,7 @@ const t = new Twit(config);
 
 //get()
 
-var params={id:'1',count:5}
+var params={id:'1',count:3}
 t.get('trends/place',params,function(err,data,res){
   var top_twit=JSON.stringify(data, undefined, 2);
   twit_stream = fs.createWriteStream('trends.json');
@@ -20,12 +20,52 @@ t.get('trends/place',params,function(err,data,res){
 /*=============================================================================/
 /*-------------------------Twits Check-----------------------------------------*/
 trending = require('./trends.json');
+console.log("calling trending")
 console.log(trending[0].trends[0]);
+
+
+console.log("/////////////////////// ")
+var array1=[]
+array1=top5()
+for (var k=0;k<5;k++)
+{
+  get(trending[0].trends[array1[k]].name)
+}
+
+
+
+
+
 //=============================================================================//
 
 
-
-
+//------------------------------------------------------//
+function top5()
+{
+  var array = [];
+  for (var i=0;i<5;i++)
+  {  c=0, no=0;
+    for (var j=0;j<trending[0].trends.length;j++)
+    {
+  if((trending[0].trends[j].tweet_volume)>c)
+      {ex=0;
+        for(var v=0;v<array.length;v++){
+          if(array[v]==j)
+          {ex=1;
+            break;}
+        }
+      if(ex==0)
+      {
+      no=j
+      c=trending[0].trends[j].tweet_volume}
+}
+}
+array.push(no)
+}
+console.log(array)
+return array;
+}
+//------------------------------------------------------//
 
 function posting()
 {
@@ -39,9 +79,10 @@ function posting()
 
 
 
-function get()
+function get(query)
 {
-    t.get('search/tweets',{q:'aman',result_type:'popular',count:5},function(err,data1,res){
+console.log(query)
+    t.get('search/tweets',{q:query,result_type:'popular',count:5},function(err,data1,res){
       if(err){
         console.log('error');
       }
@@ -55,4 +96,5 @@ function get()
         }
       }
     });
+    console.log("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
 }
